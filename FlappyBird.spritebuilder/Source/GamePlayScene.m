1ac6b727deb50e5a9072b29c186b55fc4470cd87
Mario Alcantara <mario.alcantara@gmail.com>
2014-11-19 18:01:11 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,21 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle=0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle +=delta; // delta is approximately 1/60th of a second
+    if (timeSinceObstacle>2.0f)
+    {
+         // Add a new obstacle
+        [self addObstacle];
+        
+        // Then reset the timer.
+        timeSinceObstacle=0.0f;
+    }
 }
 
 // put new methods here
